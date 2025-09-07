5ed7e4b1a7ed1af37e0d935505ea29324dc35aa8
daaoooy <y020d226y@daum.net>
2025-09-07 17:32:47 +0900

    ✨ React query 및 ProjectPage 설정
    - QueryClientProvider 설정
    - ProjectPage 컴포넌트 임시 연결 (칸반보드 구현 확인을 위해 ProjectPage을 추가해두었음)
    - React query 사용 준비

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import ProjectPage from './pages/ProjectPage';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
 function App() {
-  return;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ProjectPage />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
