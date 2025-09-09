13d19070c45a563363a24f7d159659cdd2219e8b
daaoooy <y020d226y@daum.net>
2025-09-10 03:52:20 +0900

    ✨ (#8) 프로젝트 페이지 임시 완성 (사이드바 제외)
    - 프로젝트 id를 통한 페이지 라우팅 구현
    - 헤더, 필터탭, 상단탭, 칸반보드, 구분선 컴포넌트를 조합하여 페이지 구성
    - 사이드바는 임시로 div 설정

--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -1,7 +1,26 @@
 import KanbanBoard from '@/features/kanban/components/KanbanBoard';
+import { Separator } from '@/shared/components/shadcn/separator';
+import FilterTab from '@/widgets/FilterTab';
+import Header from '@/widgets/Header';
+import TobTab from '@/widgets/TobTab';
+import { useParams } from 'react-router';
 
 function ProjectPage() {
-  return <KanbanBoard />;
+  const { projectId } = useParams<{ projectId: string }>();
+
+  return (
+    <div className="flex flex-row h-screen">
+      <div className="w-[75px] bg-gray-900 flex-shrink-0 text-white">임시 사이드바</div>
+      <div className="flex-1 flex flex-col">
+        <TobTab />
+        <Header projectId={Number(projectId)} />
+        <Separator className="bg-gray-300" />
+        <FilterTab />
+        <Separator className="bg-gray-300" />
+        <KanbanBoard />
+      </div>
+    </div>
+  );
 }
 
 export default ProjectPage;
