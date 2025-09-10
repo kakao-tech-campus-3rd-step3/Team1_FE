53ede5a3065bcdef33b2247774cf8691a8342ff1
hyemin <hyemin6647@naver.com>
2025-09-10 15:58:56 +0900

    ✨ Auth 관련 api 수정 및  토큰 재발급 api 추가

--- src/features/auth/api/authApi.ts
+++ src/features/auth/api/authApi.ts
@@ -1,22 +1,20 @@
 import { api } from '@/shared/api/axiosInstance';
+import type { KakaoLoginRequest } from '../types/auth';
 
-export interface LoginDto {
-  id: string;
-  password: string;
-}
+//카카오 로그인 (받아온 인가코드와 함께 BE에 전송)
+export const fetchKaKaoLogin = async ({ code }: KakaoLoginRequest) => {
+  const res = await api.get('auth/kakao/callback', { params: { code } });
+  return res.data;
+};
 
-// //로그인
-// export const login = async (data: LoginDto) => {
-//   const res = await api.post('/auth/login', data);
-//   return res.data;
-// };
-//로그아웃
-export const logout = async () => {
+// 로그아웃
+export const fetchLogout = async () => {
   const res = await api.post('/auth/logout');
   return res.data;
 };
-//토큰 재발급
-export const refreshToken = async () => {
+
+// 토큰 재발급
+export const fetchRefreshToken = async () => {
   const res = await api.post('/auth/refresh');
   return res.data;
 };
