0a89b7cfba3d0c45237fedc306cff31bb60b3175
hyemin <hyemin6647@naver.com>
2025-09-10 21:57:53 +0900

    ✨ 새로고침 시 토큰 재발급 로직 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AppRouter } from './router/Router';
+// import AppInitializer from './app/AppInitializer';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      {/* //아래 컴포넌트는 API와 연동이 되어있어 주석처리 했습니다.  */}
+      {/* < AppInitializer/> */}
       <AppRouter />
     </QueryClientProvider>
   );
