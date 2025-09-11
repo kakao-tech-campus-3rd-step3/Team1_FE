46836ee035a318ec83df2a0e95924711133c9cf2
daaoooy <y020d226y@daum.net>
2025-09-12 01:35:19 +0900

    ♻️ (#23) mutation 훅임을 명확히 하기 위해 네이밍 수정

--- src/pages/KakaoCallbackPage.tsx
+++ src/pages/KakaoCallbackPage.tsx
@@ -1,18 +1,17 @@
-
 import { useKakaoLoginMutation } from '@/features/auth/hooks/useKakaoLoginMutation';
 import { useEffect } from 'react';
 
 const KakaoCallbackPage = () => {
-  const {mutate: kakaoLogin} = useKakaoLoginMutation()
+  const { mutate: kakaoLoginMutation } = useKakaoLoginMutation();
 
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
     const code = params.get('code');
     if (code) {
-      kakaoLogin({ code });
+      kakaoLoginMutation({ code });
     }
-  }, [kakaoLogin]);
-  return null //백그라운드 로그인 처리용 페이지이므로 null 처리 
+  }, [kakaoLoginMutation]);
+  return null; //백그라운드 로그인 처리용 페이지이므로 null 처리
 };
 
 export default KakaoCallbackPage;
