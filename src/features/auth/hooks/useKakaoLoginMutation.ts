5424c91d594f862e5181ce5c4eabfd7c87947d97
daaoooy <y020d226y@daum.net>
2025-09-12 01:24:00 +0900

    ♻️ (#23) import 절대 경로로 수정

--- src/features/auth/hooks/useKakaoLoginMutation.ts
+++ src/features/auth/hooks/useKakaoLoginMutation.ts
@@ -1,7 +1,7 @@
 import { useMutation } from '@tanstack/react-query';
-import type { KakaoLoginRequest, KakaoLoginResponse } from '../types/authTypes';
-import { useAuthStore } from '../store/authStore';
-import { fetchKaKaoLogin } from '../api/authApi';
+import type { KakaoLoginRequest, KakaoLoginResponse } from '@/features/auth/types/authTypes';
+import { useAuthStore } from '@/features/auth/store/authStore';
+import { fetchKaKaoLogin } from '@/features/auth/api/authApi';
 
 export const useKakaoLoginMutation = () => {
   return useMutation({
--- src/features/auth/hooks/useLogoutMutation.ts
+++ src/features/auth/hooks/useLogoutMutation.ts
@@ -1,6 +1,6 @@
 import { useMutation } from '@tanstack/react-query';
-import { fetchLogout } from '../api/authApi';
-import { useAuthStore } from '../store/authStore';
+import { fetchLogout } from '@/features/auth/api/authApi';
+import { useAuthStore } from '@/features/auth/store/authStore';
 
 export const useLogoutMutation = () => {
   const clearAuth = useAuthStore((state) => state.clearAuth);
