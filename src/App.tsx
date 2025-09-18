92e8bb3df32cf45fdd43b08b0548b9c1cd28fc62
hyemin <hyemin6647@naver.com>
2025-09-18 14:49:20 +0900

    ✨ (#31) 에러 발생 시 fallback UI 표시하도록 ErrorBoundary 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AppRouter } from '@/app/routes/Router';
+import ServerErrorPage from './pages/ServerErrorPage';
+import { ErrorBoundary } from 'react-error-boundary';
 
 const queryClient = new QueryClient();
+// 아래는 에러바운더리 테스트용 코드입니다.  
+// const BuggyComponent = () => {
+//   throw new Error('😱 일부러 발생시킨 에러!');
+//   return <div>안보일거야</div>;
+// };
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      < AppInitializer/>
-      <AppRouter />
-    </QueryClientProvider>
+    <ErrorBoundary fallback={<ServerErrorPage />}>
+      <QueryClientProvider client={queryClient}>
+        {/* < AppInitializer/> */}
+        {/* <BuggyComponent /> */}
+
+        <AppRouter />
+      </QueryClientProvider>
+    </ErrorBoundary>
   );
 }
 
