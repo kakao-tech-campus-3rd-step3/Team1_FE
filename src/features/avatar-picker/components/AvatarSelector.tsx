bf942d7fe1d858a4e33dc9ee0ced556aeddb04c5
hyemin <hyemin6647@naver.com>
2025-09-19 00:59:06 +0900

    ✨ (#12) AvatarSelector 컴포넌트 구현

---
+++ src/features/avatar-picker/components/AvatarSelector.tsx
@@ -0,0 +1,28 @@
+import CurrentAvartar from '@/features/avatar-picker/components/CurrentAvatar';
+import AvartarsDrawer from '@/features/avatar-picker/components/AvatarsDrawer';
+interface AvatarSelectorProps {
+  isDrawerOpen: boolean;
+  setIsDrawerOpen: (open: boolean) => void;
+  avatarList: string[];
+  selectedAvatar: string;
+  handleAvatarSelect: (avatarUrl: string) => void;
+}
+const AvatarSelector = ({isDrawerOpen , selectedAvatar, setIsDrawerOpen, avatarList, handleAvatarSelect}: AvatarSelectorProps) => {
+  return (
+    <div className="flex flex-col items-center space-y-6 pt-10">
+      <div className="relative group">
+        <div className="absolute inset-0 bg-gradient-to-r from-boost-orange via-boost-blue to-purple-500 rounded-full blur-lg opacity-20 group-hover:opacity-30 transition-opacity duration-300 scale-110"></div>
+        <CurrentAvartar selectedAvatar={selectedAvatar} />
+        <AvartarsDrawer
+          isDrawerOpen={isDrawerOpen}
+          setIsDrawerOpen={setIsDrawerOpen}
+          avatarList={avatarList}
+          selectedAvatar={selectedAvatar}
+          handleAvatarSelect={handleAvatarSelect}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default AvatarSelector
