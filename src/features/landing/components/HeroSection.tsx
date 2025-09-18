3fd4527d3f46f6f72bc448b6e3cf99bf6d3910e7
hyemin <hyemin6647@naver.com>
2025-09-19 00:56:45 +0900

    ✨ 랜딩페이지 버튼에 추가 및 drawer 설치

--- src/features/landing/components/HeroSection.tsx
+++ src/features/landing/components/HeroSection.tsx
@@ -18,6 +18,7 @@ const HeroSection = () => {
   const options = [
     { label: '프로젝트 페이지', click: () => navigate(`/project/${123}`) },
     { label: '에러 페이지', click: () => navigate('/error') },
+    { label: '아바타 설정ru 페이지', click: () => navigate('/avartar') },
   ];
 
   return (
