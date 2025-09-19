4bbad14716364d9c34e327f0ceac53153032e46d
daaoooy <y020d226y@daum.net>
2025-09-19 15:04:26 +0900

    ✨ (#33) 모달 테스트를 위한 페이지 추가 및 router 코드 리팩터링

--- src/app/routes/Router.tsx
+++ src/app/routes/Router.tsx
@@ -7,6 +7,7 @@ import { createBrowserRouter } from 'react-router';
 import { RouterProvider } from 'react-router-dom';
 import ProtectedRoute from '@/app/routes/ProtectedRoute';
 import ServerErrorPage from '@/pages/ServerErrorPage';
+import ModalTestPage from '@/pages/ModalTestPage';
 
 export const ROUTE_PATH = {
   MAIN: '/',
@@ -14,27 +15,28 @@ export const ROUTE_PATH = {
   PROJECT: '/project/:projectId',
   MYTASK: '/my-task',
   ERROR: '/error',
+  MODAL: '/modal-test',
 };
+
 const PUBLIC_ROUTES = [
   { path: '/', element: <LandingPage /> },
   { path: '/login', element: <LoginPage /> },
   { path: '/error', element: <ServerErrorPage /> },
+  { path: ROUTE_PATH.MODAL, element: <ModalTestPage /> },
 ];
+
 const PROTECTED_ROUTES = [
   { path: '/project/:projectId', element: <ProjectPage /> },
   { path: '/my-task', element: <MyTaskPage /> },
 ];
 
 export const router = createBrowserRouter([
-  { path: ROUTE_PATH.MAIN, element: <LandingPage /> },
-  { path: ROUTE_PATH.LOGIN, element: <LoginPage /> },
-
+  // 공개 라우트
+  ...PUBLIC_ROUTES,
   {
     path: '/',
     element: <AppLayout />,
     children: [
-      // 공개 라우트
-      ...PUBLIC_ROUTES,
       // 보호된 라우트
       ...PROTECTED_ROUTES.map((route) => ({
         ...route,
