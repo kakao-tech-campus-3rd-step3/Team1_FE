98bcf57f4fa7aea49dd548135aae5cc28e788112
hyemin <hyemin6647@naver.com>
2025-09-26 23:17:31 +0900

    ✨ (#60) 절대경로로 수정, 담당자 여러명일때의 로직 추가

--- src/features/task-detail/components/ContentItem.tsx
+++ src/features/task-detail/components/ContentItem.tsx
@@ -1,14 +1,12 @@
-const ContentItem = ({
-  icon,
-  title,
-  children,
-}: {
+type ContentItemProps = {
   icon: React.ReactNode;
   title: string;
   children: React.ReactNode;
-}) => {
+};
+
+const ContentItem = ({ icon, title, children }: ContentItemProps) => {
   return (
-    <div className="flex-col">
+    <div>
       <div className="flex items-center gap-2 mb-2 text-gray-900 font-extrabold">
         {icon} <span> {title} </span>
       </div>
