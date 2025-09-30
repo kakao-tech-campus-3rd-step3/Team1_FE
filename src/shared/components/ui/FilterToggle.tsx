dc3593e55c042b8b387c77a9586be80603c36152
daaoooy <y020d226y@daum.net>
2025-09-30 16:24:03 +0900

    ✨ (#73) 상태/팀원 필터 보드 전환 기능 추가

--- src/shared/components/ui/FilterToggle.tsx
+++ src/shared/components/ui/FilterToggle.tsx
@@ -1,9 +1,18 @@
 import { Tabs, TabsList, TabsTrigger } from '@/shared/components/shadcn/tabs';
 
-const FilterToggle = () => {
+interface FilterToggleProps {
+  value: 'state' | 'team-member';
+  onChange: (value: 'state' | 'team-member') => void;
+}
+
+const FilterToggle = ({ value, onChange }: FilterToggleProps) => {
   return (
     <div className="flex max-w-sm flex-col gap-6">
-      <Tabs defaultValue="state">
+      <Tabs
+        value={value}
+        defaultValue="state"
+        onValueChange={(val) => onChange(val as 'state' | 'team-member')}
+      >
         <TabsList>
           <TabsTrigger value="state">상태</TabsTrigger>
           <TabsTrigger value="team-member">팀원</TabsTrigger>
