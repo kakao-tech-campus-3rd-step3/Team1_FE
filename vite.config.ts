5173a08b47015fcc8bf39ae708567b7bea23d4b0
daaoooy <y020d226y@daum.net>
2025-10-03 18:36:39 +0900

    🔧 Api 요청 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://api.boost.ai.kr',
+        target: 'https://api.boost.ai.kr',
         changeOrigin: true,
         secure: false,
         // rewrite: (path) => path.replace(/^\/api/, ''),
