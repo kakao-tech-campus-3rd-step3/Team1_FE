27d9262595f826320b6e477d6f9840025e66e6af
daaoooy <y020d226y@daum.net>
2025-10-21 02:04:56 +0900

    🚚 폴더명 TaskCreateModalContent -> TaskCreateModal

--- src/features/task/components/TaskCreateModal/TaskCreateModalContent.tsx
+++ src/features/task/components/TaskCreateModal/TaskCreateModalContent.tsx
@@ -6,39 +6,85 @@ import { FormField } from '@/shared/components/ui/Form/FormField';
 import { useModal } from '@/shared/hooks/useModal';
 import { toggleArrayItem } from '@/shared/utils/arrayUtils';
 import { cn } from '@/shared/lib/utils';
-import { Calendar, User, Tag, Siren, FileText, Loader, Check, NotebookPen } from 'lucide-react';
+import {
+  Calendar,
+  User,
+  Tag,
+  Siren,
+  FileText,
+  Loader,
+  Check,
+  NotebookPen,
+  Folder,
+} from 'lucide-react';
 import StatusButtons from '@/features/task/components/TaskCreateModalContent/StatusButtons';
 import UrgentToggle from '@/features/task/components/TaskCreateModalContent/UrgentToggle';
 import AssigneeDropdown from '@/features/task/components/TaskCreateModalContent/AssigneeDropdown';
 import TagInput from '@/features/task/components/TaskCreateModalContent/TagInput';
 import { useTaskForm } from '@/features/task/hooks/useTaskForm';
-import type { CreateTaskInput } from '@/features/task/schemas/taskSchema';
 import { statusList } from '@/features/board/types/boardTypes';
+import { useEffect } from 'react';
+import { useProjectsQuery } from '@/features/project/hooks/useProjectsQuery';
+import { useCreateTaskMutation } from '@/features/task/hooks/useCreateTaskMutation';
+import toast from 'react-hot-toast';
+import ProjectDropdown from '@/features/task/components/TaskCreateModalContent/ProjectSelect';
 
 interface TaskCreateModalContentProps {
-  onConfirm: (taskData: CreateTaskInput) => Promise<void> | void;
+  isMyTask: boolean;
+  projectId?: string;
 }
 
-const TaskCreateModalContent = ({ onConfirm }: TaskCreateModalContentProps) => {
+const TaskCreateModalContent = ({
+  isMyTask,
+  projectId: propProjectId,
+}: TaskCreateModalContentProps) => {
   const inputClasses = 'hover:bg-gray-200 focus:ring-transparent h-11 label2-regular';
   const { resetModal } = useModal();
-  const { form, handleConfirm, isLoading } = useTaskForm(onConfirm);
+  const { data: projects } = useProjectsQuery();
+
+  const { form, handleConfirm, isLoading } = useTaskForm(async (taskData) => {
+    if (!selectedProjectId) {
+      toast.error('프로젝트를 선택해주세요.');
+      return;
+    }
+    createTask(taskData);
+    toast.success('할 일이 성공적으로 생성되었습니다!');
+    resetModal();
+  });
+
   const {
     register,
     setValue,
     watch,
     formState: { errors },
   } = form;
-
   const assignees = watch('assignees');
   const tags = watch('tags') || [];
   const status = watch('status');
   const urgent = watch('urgent') || false;
+  const selectedProjectId = watch('projectId') || propProjectId;
+  const { mutate: createTask } = useCreateTaskMutation(selectedProjectId ?? '');
+
+  useEffect(() => {
+    if (isMyTask && !watch('projectId') && projects?.length) {
+      setValue('projectId', projects[0].id);
+    }
+  }, [isMyTask, projects, setValue, watch]);
 
   return (
     <>
       <div className="flex flex-col gap-8 py-4 max-h-[400px] overflow-y-auto px-1">
-        {/* 할 일 제목 */}
+        {isMyTask && (
+          <FormField icon={Folder} required label="프로젝트" error={errors.projectId?.message}>
+            <ProjectDropdown
+              selectedProjectId={watch('projectId')}
+              projects={projects || []}
+              onProjectSelect={(id) => setValue('projectId', id, { shouldValidate: true })}
+            />
+          </FormField>
+        )}
+
+        {/* 제목 */}
         <FormField icon={FileText} required label="제목" error={errors.title?.message}>
           <Input
             {...register('title')}
@@ -115,19 +161,14 @@ const TaskCreateModalContent = ({ onConfirm }: TaskCreateModalContentProps) => {
       {/* 하단 버튼 영역 (취소, 생성) */}
       <DialogFooter className="gap-2 pt-4 border-t border-gray-300">
         <Button
-          onClick={() => resetModal()}
+          onClick={resetModal}
           variant="outline"
           disabled={isLoading}
           className="px-6 border-gray-400"
         >
           취소
         </Button>
-        <Button
-          onClick={handleConfirm}
-          className="px-6 bg-boost-blue hover:boost-blue-hover"
-          // TODO: disabled 처리를 할지, error 메세지를 보여줄지 고민 중입니다!
-          // disabled={isLoading || !form.formState.isValid}
-        >
+        <Button onClick={handleConfirm} className="px-6 bg-boost-blue hover:boost-blue-hover">
           {isLoading ? '생성 중...' : '할 일 생성'}
         </Button>
       </DialogFooter>
