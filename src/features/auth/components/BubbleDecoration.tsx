5dc1242e04a2dbb343cd33fca171f5ed6009c0ad
daaoooy <y020d226y@daum.net>
2025-10-22 05:03:15 +0900

    🎨 <img> 태그의 alt 속성 추가

--- src/features/auth/components/BubbleDecoration.tsx
+++ src/features/auth/components/BubbleDecoration.tsx
@@ -11,7 +11,7 @@ const BubbleDecoration = () => {
       custom={5}
     >
       <div className="flex flex-row items-center gap-1 border border-boost-blue-dark/30 text-gray-700 px-3 py-2 rounded-full shadow-md label1-regular">
-        <img src={BoostRocket} alt="" className="w-5.5" />
+        <img src={BoostRocket} alt="boost-rocket" className="w-5.5" />
         5초 안에 회원가입
       </div>
     </motion.div>
