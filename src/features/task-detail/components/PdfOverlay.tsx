59d1de2ca7c71b4ad964188bc41d4a15a76e0862
hyemin <hyemin6647@naver.com>
2025-10-25 16:46:32 +0900

    ♻️ (#179) 핀 위치 오류 해결을 위해 frame motion 삭제

--- src/features/task-detail/components/PdfOverlay.tsx
+++ src/features/task-detail/components/PdfOverlay.tsx
@@ -1,7 +1,6 @@
 import Profile from '@/shared/assets/images/avatars/78.png';
 import type { PageSize } from '@/features/task-detail/types/pdfTypes';
 import type { FileInfo } from '@/features/comment/types/commentTypes';
-import { motion } from 'framer-motion';
 interface OverlayProps {
   pins: FileInfo[];
   currentPin: FileInfo | null;
@@ -27,7 +26,7 @@ const Overlay = ({ pins, currentPin, pageNumber, zoom, pageSize, onClick }: Over
               style={{
                 left: `${left}%`,
                 top: `${top}%`,
-                transform: `translate(-50%, -50%) scale(${zoom})`,
+                transform: `translate(50%, -120%) scale(${zoom})`,
               }}
             >
               <img
@@ -43,28 +42,21 @@ const Overlay = ({ pins, currentPin, pageNumber, zoom, pageSize, onClick }: Over
         (() => {
           const left = (currentPin.fileX ? currentPin.fileX / pageSize.width : 0) * 100;
           const top = 100 - (currentPin.fileY ? currentPin.fileY / pageSize.height : 0) * 100;
-
           return (
-            <motion.div
-              layout
-              transition={{
-                layout: { duration: 0.25, ease: 'easeInOut' },
-              }}
-              className="absolute w-8 h-8 rounded-[50%_50%_50%_0] -rotate-45 border-2 border-boost-orange shadow-md overflow-hidden cursor-pointer bg-boost-orange opacity-80"
+            <div
+              className="absolute w-8 h-8 rounded-[50%_50%_50%_0] -rotate-45 border-2 border-boost-orange shadow-md overflow-hidden cursor-pointer bg-boost-orange opacity-80 transition-all duration-300 ease-in-out"
               style={{
                 left: `${left}%`,
                 top: `${top}%`,
-                transform: `translate(-50%, -50%) scale(${zoom})`,
+                transform: `translate(50%, -120%) scale(${zoom})`,
               }}
-              initial={{ scale: 0, opacity: 0 }}
-              animate={{ scale: 1.1, opacity: 1 }}
             >
               <img
                 src={Profile}
                 alt="current pin"
                 className="w-full h-full object-cover rotate-[45deg]"
               />
-            </motion.div>
+            </div>
           );
         })()}
     </div>
