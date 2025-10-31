8b5b620c94168d68ac28152e24a411f26707a2e8
hyemin <hyemin6647@naver.com>
2025-11-01 01:12:35 +0900

    🔥 (#207) 중복되는 파일 삭제

--- src/features/avatar-picker/api/avatarApi.ts
+++ src/features/avatar-picker/api/avatarApi.ts
@@ -1,11 +1,11 @@
-import type { AvatarInfo } from '@/features/user/types/userTypes';
-import api from '@/shared/api/axiosInstance';
+// import type { AvatarInfo } from '@/features/user/types/userTypes';
+// import api from '@/shared/api/axiosInstance';
 
-export const avatarApi = {
-  uploadAvatar: async (avatarInfo: AvatarInfo): Promise<void> => {
-    await api.put('/members/me/avatar', {
-      avatar: avatarInfo.avatarId,
-      backgroundColor: avatarInfo.backgroundColor,
-    });
-  },
-};
+// export const avatarApi = {
+//   uploadAvatar: async (avatarInfo: AvatarInfo): Promise<void> => {
+//     await api.put('/members/me/avatar', {
+//       avatar: avatarInfo.avatarId,
+//       backgroundColor: avatarInfo.backgroundColor,
+//     });
+//   },
+// };
--- src/features/avatar-picker/hooks/useAvatarSaveMutation.ts
+++ src/features/avatar-picker/hooks/useAvatarSaveMutation.ts
@@ -1,16 +1,15 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { avatarApi } from '@/features/avatar-picker/api/avatarApi';
-import type { AvatarInfo } from '@/features/user/types/userTypes';
+// import { useMutation, useQueryClient } from '@tanstack/react-query';
+// import type { AvatarInfo } from '@/features/user/types/userTypes';
+// import { settingsApi } from '@/features/settings/api/settingsApi';
 
-export const useAvatarSaveMutation = () => {
-  const queryClient = useQueryClient();
-  return useMutation<void, Error, AvatarInfo>({
-    mutationFn: async ({ avatarId, backgroundColor }) => {
-      console.log(avatarId, backgroundColor);
-      await avatarApi.uploadAvatar({ avatarId: avatarId, backgroundColor });
-    },
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['myInfo'] });
-    },
-  });
-};
+// export const useAvatarSaveMutation = () => {
+//   const queryClient = useQueryClient();
+//   return useMutation<void, Error, AvatarInfo>({
+//     mutationFn: async ({ avatar, backgroundColor }) => {
+//       await settingsApi.updateAvatar({ avatar: avatar, backgroundColor });
+//     },
+//     onSuccess: () => {
+//       queryClient.invalidateQueries({ queryKey: ['myInfo'] });
+//     },
+//   });
+// };
