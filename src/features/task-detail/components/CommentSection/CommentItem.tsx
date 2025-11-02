6bcb009a2ac8c413aa43972bf77b7a5407673ff8
hyemin <hyemin6647@naver.com>
2025-11-02 21:56:26 +0900

    ♻️ (#212) 댓글 작성자일때만 더보기 보이도록 수정

--- src/features/task-detail/components/CommentSection/CommentItem.tsx
+++ src/features/task-detail/components/CommentSection/CommentItem.tsx
@@ -11,6 +11,7 @@ import { cn } from '@/shared/lib/utils';
 import type { CommentUIType } from '@/features/comment/types/commentTypes';
 import { getAvatarSrc } from '@/features/avatar-picker/utils/avatarUtils';
 import type { FileInfo } from '@/features/task-detail/types/taskDetailType';
+import { useAuthStore } from '@/features/auth/store/authStore';
 
 interface CommentItemProps {
   comment: CommentUIType;
@@ -21,10 +22,17 @@ interface CommentItemProps {
 
 const CommentItem = ({ comment, onEdit, onDelete, onSelectPin }: CommentItemProps) => {
   const isAnonymous = comment.isAnonymous;
+  const { user } = useAuthStore();
+  const isAuthor = user?.id === comment.authorInfo.id;
   return (
     <div className="flex py-3">
       <div className="flex-1">
-        <div className="bg-gray-200 rounded-xl px-4 py-3 shadow-sm relative">
+        <div
+          onClick={() => {
+            if (onSelectPin) onSelectPin(comment.fileInfo ?? null);
+          }}
+          className="bg-gray-200 rounded-xl px-4 py-3 shadow-sm relative"
+        >
           <div className="flex items-center justify-between">
             <div className="flex items-center gap-2 pb-3">
               <Avatar
@@ -56,52 +64,50 @@ const CommentItem = ({ comment, onEdit, onDelete, onSelectPin }: CommentItemProp
               <span className="label1-bold text-sm text-gray-800">
                 {isAnonymous ? '익명' : comment.authorInfo.name}
               </span>
-              {comment.isPinned && (
-                <Pin
-                  onClick={() => {
-                    if (onSelectPin) onSelectPin(comment.fileInfo ?? null);
-                  }}
-                  className="h-3.5 w-3.5 text-boost-blue cursor-pointer"
-                />
-              )}
+              {comment.isPinned && <Pin className="h-3.5 w-3.5 text-boost-blue cursor-pointer" />}
             </div>
-
-            <div className="flex items-center gap-1">
+            {isAuthor ? (
+              <div className="flex items-center gap-1">
+                <span className="text-xs text-gray-500">{comment.timeAgo}</span>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <button
+                      className="p-1 hover:bg-gray-100 rounded-md"
+                      onClick={(e) => e.stopPropagation()}
+                    >
+                      <EllipsisVertical className="w-4 h-4 text-gray-600" />
+                    </button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent
+                    align="end"
+                    className="w-32 bg-white rounded-lg shadow-md border border-gray-200"
+                  >
+                    <DropdownMenuGroup>
+                      <DropdownMenuItem
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          onEdit?.(comment);
+                        }}
+                        className="px-3 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer"
+                      >
+                        수정
+                      </DropdownMenuItem>
+                      <DropdownMenuItem
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          onDelete?.(comment.commentId);
+                        }}
+                        className="px-3 py-2 text-sm text-red-600 hover:bg-gray-100 cursor-pointer"
+                      >
+                        삭제
+                      </DropdownMenuItem>
+                    </DropdownMenuGroup>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </div>
+            ) : (
               <span className="text-xs text-gray-500">{comment.timeAgo}</span>
-
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <button className="p-1 hover:bg-gray-100 rounded-md">
-                    <EllipsisVertical className="w-4 h-4 text-gray-600" />
-                  </button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent
-                  align="end"
-                  className="w-32 bg-white rounded-lg shadow-md border border-gray-200"
-                >
-                  <DropdownMenuGroup>
-                    <DropdownMenuItem
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        if (onEdit) onEdit(comment);
-                      }}
-                      className="px-3 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer"
-                    >
-                      수정
-                    </DropdownMenuItem>
-                    <DropdownMenuItem
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        if (onDelete) onDelete(comment.commentId);
-                      }}
-                      className="px-3 py-2 text-sm text-red-600 hover:bg-gray-100 cursor-pointer"
-                    >
-                      삭제
-                    </DropdownMenuItem>
-                  </DropdownMenuGroup>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </div>
+            )}
           </div>
 
           <p className="mt-1 text-sm text-gray-800">{comment.content}</p>
