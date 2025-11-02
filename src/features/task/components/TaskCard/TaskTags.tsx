7b574d8e4d99e4c592f0ae0b7485e4c0afe47735
daaoooy <y020d226y@daum.net>
2025-11-02 19:36:07 +0900

    ♻️ (#204) 리팩터링한 태그 유틸 함수 적용 및 디자인 수정

--- src/features/task/components/TaskCard/TaskTags.tsx
+++ src/features/task/components/TaskCard/TaskTags.tsx
@@ -1,6 +1,10 @@
-import { Badge } from '@/shared/components/shadcn/badge';
+import {
+  generateTags,
+  getColorStyleForTag,
+  generatePastelColor,
+} from '@/features/tag/utils/tagUtils';
 import { cn } from '@/shared/lib/utils';
-import { generateTags, getColorForTag } from '@/shared/utils/tagUtils';
+import { Badge } from '@/shared/components/shadcn/badge';
 import type { TaskDetail, TaskListItem } from '@/features/task/types/taskTypes';
 
 interface TaskTagsProps {
@@ -10,18 +14,28 @@ interface TaskTagsProps {
 
 const TaskTags = ({ task, projectName }: TaskTagsProps) => {
   const tags = generateTags(task);
+  const projectTagStyle = projectName ? generatePastelColor(projectName, true) : null;
 
   return (
     <div className="flex flex-wrap gap-1 m-1">
       {/* 프로젝트 이름 태그 */}
-      {projectName && (
-        <Badge className={cn('text-xs bg-gray-200 text-gray-700')}>{projectName}</Badge>
+      {projectName && projectTagStyle && (
+        <Badge
+          key={projectName}
+          className="rounded-full"
+          style={{
+            ...projectTagStyle,
+            border: cn('1px solid', projectTagStyle.borderColor),
+          }}
+        >
+          {projectName}
+        </Badge>
       )}
 
       {/* 기존 태그 */}
       {tags.map((tag) => (
-        <Badge key={tag} className={cn('text-xs', getColorForTag(tag))}>
-          {tag}
+        <Badge key={tag.tagId} style={getColorStyleForTag(tag)}>
+          {tag.name}
         </Badge>
       ))}
     </div>
