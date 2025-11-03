8df3dd2529788f752e6edd97b8e95e7de2ef4880
daaoooy <y020d226y@daum.net>
2025-11-04 08:02:39 +0900

    ✨ (#235) 보드 검색 store 구현

---
+++ src/features/board/store/useBoardSearchStore.ts
@@ -0,0 +1,30 @@
+import { create } from 'zustand';
+import { BOARD_KEYS, type BoardKey } from '@/features/board/constants/boardConstants';
+
+interface BoardSearchState {
+  searchMap: Record<BoardKey, string>;
+  setSearch: (boardKey: BoardKey, value: string) => void;
+  clearOtherBoardSearch: (currentBoardKey: BoardKey) => void;
+}
+
+export const useBoardSearchStore = create<BoardSearchState>((set) => ({
+  searchMap: {
+    [BOARD_KEYS.MY_TASKS]: '',
+    [BOARD_KEYS.PROJECT_STATUS]: '',
+    [BOARD_KEYS.PROJECT_MEMBER]: '',
+  },
+
+  setSearch: (boardKey, value) =>
+    set((state) => ({
+      searchMap: { ...state.searchMap, [boardKey]: value },
+    })),
+
+  clearOtherBoardSearch: (currentBoardKey) =>
+    set((state) => {
+      const newMap: Record<BoardKey, string> = { ...state.searchMap };
+      Object.keys(newMap).forEach((key) => {
+        if (key !== currentBoardKey) newMap[key as BoardKey] = '';
+      });
+      return { searchMap: newMap };
+    }),
+}));
