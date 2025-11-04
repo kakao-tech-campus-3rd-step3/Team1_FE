6fff9e82aa18d7de822cb6c6a40c9ba71aaef8b3
daaoooy <y020d226y@daum.net>
2025-11-04 10:01:38 +0900

    ♻️ (#220) 아바타 수정 응답 반환하도록 수정

--- src/features/settings/api/settingsApi.ts
+++ src/features/settings/api/settingsApi.ts
@@ -1,22 +1,26 @@
 import api from '@/shared/api/axiosInstance';
 import type { MyInfoResponse } from '@/features/settings/types/settingsTypes';
 import type { AvatarInfo } from '@/features/user/types/userTypes';
+
 export const settingsApi = {
   // 회원 조회
   getMyInfo: async (): Promise<MyInfoResponse> => {
     const { data } = await api.get('/members/me');
     return data;
   },
+
   // 이름 수정
   updateName: async (name: string): Promise<void> => {
     await api.put('/members/me/name', { name });
   },
+
   // 아바타 수정
-  updateAvatar: async ({ avatar, backgroundColor }: AvatarInfo): Promise<void> => {
-    await api.put('/members/me/avatar', {
-      avatar: avatar,
-      backgroundColor: backgroundColor,
+  updateAvatar: async ({ avatar, backgroundColor }: AvatarInfo): Promise<AvatarInfo> => {
+    const res = await api.put('/members/me/avatar', {
+      avatar,
+      backgroundColor,
     });
+    return res.data;
   },
 
   // 회원 탈퇴
