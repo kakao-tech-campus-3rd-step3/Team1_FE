45ccaf95ee7fac231b06287b3b3371c7f2fbc68e
hyemin <hyemin6647@naver.com>
2025-11-05 15:51:04 +0900

    🎨 (#252) toast 메시지 수정

--- src/features/settings/hooks/useUpdateNotificationSettingsMutation.ts
+++ src/features/settings/hooks/useUpdateNotificationSettingsMutation.ts
@@ -32,7 +32,7 @@ export const useUpdateNotificationSettingsMutation = (
     onSuccess: (data) => {
       toast.success(
         data.enabled
-          ? '서비스 전체 알림이 서비스 알림이 켜졌습니다'
+          ? '서비스 알림이 켜졌습니다'
           : '서비스 전체 알림이 꺼졌습니다',
       );
     },
