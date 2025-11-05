7ed55b1e735ccf527349f349e04ce08fd240844c
daaoooy <y020d226y@daum.net>
2025-11-05 17:18:05 +0900

    ♻️ (#247) 멤버 추방 시 할 일 카드 UI 오류 수정

--- src/features/task/components/TaskCard/AssigneesList.tsx
+++ src/features/task/components/TaskCard/AssigneesList.tsx
@@ -6,35 +6,32 @@ import {
   TooltipTrigger,
 } from '@/shared/components/shadcn/tooltip';
 import { cn } from '@/shared/lib/utils';
-import { useProjectMembersQuery } from '@/features/project/hooks/useProjectMembersQuery';
 import { getAvatarSrc } from '@/features/avatar-picker/utils/avatarUtils';
+import type { Member } from '@/features/user/types/userTypes';
 
 interface AssigneeAvatarProps {
-  projectId: string;
-  assigneeId: string;
+  assignee: Member;
 }
 
-const AssigneesList = ({ projectId, assigneeId }: AssigneeAvatarProps) => {
-  const { data: projectMembers } = useProjectMembersQuery(projectId);
-  const member = projectMembers?.find((m) => m.id === assigneeId);
+const AssigneesList = ({ assignee }: AssigneeAvatarProps) => {
   return (
     <TooltipProvider>
       <Tooltip>
         <TooltipTrigger asChild>
           <Avatar
             style={{
-              backgroundColor: member?.backgroundColor,
+              backgroundColor: assignee?.backgroundColor,
             }}
             className={cn(
               'flex items-center justify-center w-7 h-7 cursor-pointer ring-2 ring-white',
             )}
           >
-            <AvatarFallback>{member?.name[0] ?? '?'}</AvatarFallback>
-            <AvatarImage src={getAvatarSrc(member)} className="w-6 h-6" />
+            <AvatarFallback>{assignee?.name[0] ?? '?'}</AvatarFallback>
+            <AvatarImage src={getAvatarSrc(assignee)} className="w-6 h-6" />
           </Avatar>
         </TooltipTrigger>
         <TooltipContent side="top">
-          <p>{member?.name ?? assigneeId}</p>
+          <p>{assignee?.name ?? assignee.id}</p>
         </TooltipContent>
       </Tooltip>
     </TooltipProvider>
