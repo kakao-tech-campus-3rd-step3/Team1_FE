67eb744e683eda59c1c9f2e0bd5720ef3970f68a
hyemin <hyemin6647@naver.com>
2025-11-05 12:27:25 +0900

    ✨ (#255) 할일상세 스토어에 초기화 함수 추가

--- src/features/task-detail/store/useTaskDetailStore.ts
+++ src/features/task-detail/store/useTaskDetailStore.ts
@@ -1,26 +1,50 @@
 import { create } from 'zustand';
 import type { FileInfo, PinWithAuthor } from '@/features/task-detail/types/taskDetailType';
+
 interface TaskDetailState {
   selectedFile: FileInfo | null;
   currentPin: PinWithAuthor | null;
   pins: PinWithAuthor[];
   isPdfOpen: boolean;
   isAnonymous: boolean;
+
   setIsAnonymous: (val: boolean) => void;
   setSelectedFile: (fileInfo: FileInfo | null) => void;
   setPins: (pins: PinWithAuthor[]) => void;
   setCurrentPin: (pin: PinWithAuthor | null) => void;
   togglePdf: (open: boolean) => void;
+
+  clearCurrentPin: () => void;// 현재 핀의 상태 초기화
+  clearFileState: () => void; //PDF 닫기 시 사용
+  resetAll: () => void; // 뒤로가기 시 전체 초기화
 }
-export const useTaskDetailStore = create<TaskDetailState>((set) => ({
+
+const initialState = {
   selectedFile: null,
   currentPin: null,
   pins: [],
   isPdfOpen: false,
   isAnonymous: false,
+};
+
+export const useTaskDetailStore = create<TaskDetailState>((set) => ({
+  ...initialState,
+
   setIsAnonymous: (val) => set({ isAnonymous: val }),
   setSelectedFile: (selectedFile) => set({ selectedFile }),
   setPins: (pins) => set({ pins }),
   setCurrentPin: (currentPin) => set({ currentPin }),
   togglePdf: (isPdfOpen) => set({ isPdfOpen }),
+
+  clearCurrentPin: () => set({ currentPin: null }),
+
+  clearFileState: () =>
+    set({
+      selectedFile: null,
+      currentPin: null,
+      pins: [],
+      isPdfOpen: false,
+    }),
+
+  resetAll: () => set(initialState),
 }));
